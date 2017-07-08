090fd903f55adb0df5dd3070c95221b84cc224b4
Mrfalco <the_jb@bellsouth.net>
2017-07-08 02:50:50 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var activePlot = 1;
 var day = 1;
 var year = 1;
 var season = "Summer";
+var grown = 0;
+var selectedPlant = 0;
 var plot = {
 	growing1:"Nothing",
 	growing2:"Nothing",
@@ -12,8 +14,19 @@ var plot = {
 	growing7:"Nothing",
 	growing8:"Nothing",
 	growing9:"Nothing",
+	time1:-1,
+	time2:-1,
+	time3:-1,
+	time4:-1,
+	time5:-1,
+	time6:-1,
+	time7:-1,
+	time8:-1,
+	time9:-1,
 	
 }
+
+var plants = [["wheat", 8,"Fall", -2],["Grapes", 12,"Spring",-3]];
 
 function activeMenu(page){//This is where the menu tabs change
 	document.getElementById ("farmWindow").className = 'hide';//SHUT. DOWN. EVERYTHING.
@@ -52,25 +65,55 @@ function updatePlotDisplay(){
 	document.getElementById ("plotGrowing").innerHTML = plot["growing" + activePlot];
 	document.getElementById ("plotNum").innerHTML = activePlot;
 }
+
+function changePlant(id){
+	document.getElementById ("sCropName").innerHTML = plants[id][0];
+	document.getElementById ("sCropTime").innerHTML = plants[id][1] + " days";
+	selectedPlant = id;
+}
+
+function plant(){
+	plot["growing" + activePlot] = plants[selectedPlant][0];
+	if (season == plants[selectedPlant][2]){
+		plot["time" + activePlot] = plants[selectedPlant][1] - plants[selectedPlant][3];
+	}
+	else {
+		plot["time" + activePlot] = plants[selectedPlant][1];
+	}
+	updatePlotDisplay()
+}
 	
 window.setInterval(function(){//runs every 10 sec
 day += 1;
+var x = 9;
 if (day > 90){
 	day = 1;
-	if (season = "Summer"){
+	if (season == "Summer"){
 		season = "Fall";
 	}
-	if (season = "Fall"){
+	if (season == "Fall"){
 		season = "Winter";
 	}
-	if (season = "Winter"){
+	if (season == "Winter"){
 		season = "Spring";
 		year += 1;
 	}
-	if (season = "Spring"){
+	if (season == "Spring"){
 		season = "Summer";
 	}
 
+}
+while(x != 0){
+	if (plot["time" + x] != -1){
+		if (plot["time" + x] == 0){
+			grown += 1;
+			plot["growing" + x] = "Nothing";
+			updatePlotDisplay();
+		}
+		plot["time" + x] -= 1;
+		document.getElementById ("temp").innerHTML = grown;
+	}	
+	x -= 1;
 }
 document.getElementById ("season").innerHTML = season;
 document.getElementById ("day").innerHTML = day;
